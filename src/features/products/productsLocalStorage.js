6f275246e2a28529e8bafe34b5cbfd8b861a2403
lkkolosowski <lkkolosowski@gmail.com>
2023-06-18 14:01:57 +0200

    remove example product list

--- src/features/products/productsLocalStorage.js
+++ src/features/products/productsLocalStorage.js
@@ -1,9 +1,7 @@
-import exampleProducts from "./exampleProducts.json";
-
 const localStorageProductsKey = "products";
 
 export const saveProductsInLocalStorage = (products) =>
   localStorage.setItem(localStorageProductsKey, JSON.stringify(products));
 
 export const getProductsFromLocalStorage = () =>
-  JSON.parse(localStorage.getItem(localStorageProductsKey)) || exampleProducts;
+  JSON.parse(localStorage.getItem(localStorageProductsKey)) || [];
